e23d8140a6e39d6b76fd1e664d5ff306bba1b386
Meitar Basson <meitarbasson@Meitars-iMac.local>
2016-05-25 18:56:18 +0300

    Added files for multiples

--- multiplesApp/ViewController.swift
+++ multiplesApp/ViewController.swift
@@ -9,17 +9,72 @@
 import UIKit
 
 class ViewController: UIViewController {
+    // Properties
+    var last: Int = 0
+    var add: Int = 0
+    var sum: Int = 0
+    var max: Int = 0
+    
+    // Outlets
+    @IBOutlet weak var logo: UIImageView!
+    @IBOutlet weak var addText: UITextField!
+    @IBOutlet weak var startPlay: UIButton!
+    
+    @IBOutlet weak var addButton: UIButton!
+    @IBOutlet weak var display: UILabel!
+    
+    // Actions
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBAction func play(sender: UIButton!){
+        if checkIfEntered(){
+            logo.hidden = true
+            addText.hidden = true
+            startPlay.hidden = true
+        
+            addButton.hidden = false
+            display.hidden = false
+            
+            max = Int(addText.text!)! * 20
+            add = Int(addText.text!)!
+            
+            calculation()
+            
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func add(sender: UIButton!){
+       calculation()
     }
-
-
+    
+    
+    func checkIfEntered() -> Bool{
+        if addText.text != "" && addText != nil{
+            return true
+        }
+        return false
+    }
+    
+    func resetGame(){
+        sum = 0
+        last = 0
+        logo.hidden = false
+        addText.hidden = false
+        startPlay.hidden = false
+        
+        addButton.hidden = true
+        display.hidden = true
+    }
+    
+    func calculation(){
+        if sum < max {
+            sum = last + add
+            display.text = "\(last) + \(add) = \(sum)"
+            last = last + add
+        }else{
+            resetGame()
+        }
+    }
+    
+    
 }
 
